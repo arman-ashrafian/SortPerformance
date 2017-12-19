cd0a914d0fa6a67a23de216cecdfd431f300b443
Arman Ashrafian <ashy63@gmail.com>
2017-12-19 10:39:52 -0800

    added heap & merge sort

--- sort.py
+++ sort.py
@@ -23,6 +23,8 @@ def quick_sort(arr, low, high, part):
     stack[top] = low
     top = top + 1
     stack[top] = high
+
+    counter += 7
  
     # Keep popping from stack while is not empty
     while top >= 0:
@@ -32,13 +34,13 @@ def quick_sort(arr, low, high, part):
         top = top - 1
         low = stack[top]
         top = top - 1
+
+        counter += 4
  
         # Set pivot element at its correct position in
         # sorted array
-        p = part( arr, low, high )
-
-        if(p == partition1): counter += 8
-        else: counter += 10 
+        p, c = part( arr, low, high )
+        counter += c
  
         # If there are elements on left side of pivot,
         # then push left side to stack
@@ -47,6 +49,7 @@ def quick_sort(arr, low, high, part):
             stack[top] = low
             top = top + 1
             stack[top] = p - 1
+            counter += 4
  
         # If there are elements on right side of pivot,
         # then push right side to stack
@@ -55,49 +58,152 @@ def quick_sort(arr, low, high, part):
             stack[top] = p + 1
             top = top + 1
             stack[top] = high
+            counter += 4
 
-        counter += 16
     return counter
 
 # picks first value for pivot
 def partition1(arr, low, high):
+    counter = 0
     pivot = arr[low]
     border = low
-
+    counter += 2
     for i in range(low, high + 1):
+        counter += 1
         if arr[i] < pivot:
             border += 1
             arr[i], arr[border] = arr[border], arr[i]
+            counter += 2
+
     arr[low], arr[border] = arr[border], arr[low]
+    counter += 1
     
-    return border
+    return (border, counter)
 
 # picks middle value for pivot
 def partition2(arr, low, high):
+    counter = 0
     mid = (high + low) // 2
     pivot = arr[mid]
     arr[mid], arr[low] = arr[low], arr[mid]
     border = low
+    counter += 4
 
     for i in range(low, high + 1):
+        counter += 1
         if arr[i] < pivot:
             border += 1
             arr[i], arr[border] = arr[border], arr[i]
+            counter += 2
     arr[low], arr[border] = arr[border], arr[low]
-    
-    return border   
+    counter += 1
+
+    return (border, counter)   
 
 # picks middle value for pivot
 def partition3(arr, low, high):
+    counter = 0
     pivotIndex = random.randint(low, high)
     pivot = arr[pivotIndex]
     arr[pivotIndex], arr[low] = arr[low], arr[pivotIndex]
     border = low
+    counter += 4
 
     for i in range(low, high + 1):
+        counter += 1
         if arr[i] < pivot:
             border += 1
             arr[i], arr[border] = arr[border], arr[i]
+            counter += 2
     arr[low], arr[border] = arr[border], arr[low]
-    
-    return border 
+    counter += 1
+
+    return (border, counter)   
+
+# sorts arr in place
+# returns number of statements executed
+def merge_sort(arr):
+    if len(arr) <= 1: return 1
+
+    counter = 0
+
+    mid = len(arr)//2
+    lefthalf = arr[:mid]
+    righthalf = arr[mid:]
+
+    counter += 3
+
+    counter += merge_sort(lefthalf)
+    counter += merge_sort(righthalf)
+
+    i=0
+    j=0
+    k=0
+    while i < len(lefthalf) and j < len(righthalf):
+        if lefthalf[i] < righthalf[j]:
+            arr[k]=lefthalf[i]
+            i=i+1
+        else:
+            arr[k]=righthalf[j]
+            j=j+1
+        k=k+1
+        counter += 8
+
+    while i < len(lefthalf):
+        arr[k]=lefthalf[i]
+        i=i+1
+        k=k+1
+        counter += 4
+
+    while j < len(righthalf):
+        arr[k]=righthalf[j]
+        j=j+1
+        k=k+1
+        counter += 4
+
+    return counter
+
+# To heapify subtree rooted at index i.
+# returns statements executed
+def heapify(arr, n, i):
+    counter = 0
+
+    largest = i  
+    l = 2 * i + 1     
+    r = 2 * i + 2     
+    counter += 3
+
+    if l < n and arr[i] < arr[l]:
+        counter += 2
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        counter += 2
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        counter += 2
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+ 
+        counter += heapify(arr, n, largest)
+    return counter
+ 
+# sorts arr in place
+# returns statements executed
+def heap_sort(arr):
+    counter = 0
+    n = len(arr)
+    counter+=1
+ 
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        counter += heapify(arr, n, i)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        counter+=1
+        counter += heapify(arr, i, 0)
+
+    return counter
